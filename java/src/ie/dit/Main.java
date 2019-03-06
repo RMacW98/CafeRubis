c45ae834baeb005a1f1b955352582da0e265af4b
RMacW98 <47107544+RMacW98@users.noreply.github.com>
2019-03-06 12:54:42 +0000

    Add files via upload

---
+++ java/src/ie/dit/Main.java
@@ -0,0 +1,18 @@
+package ie.dit;
+
+public class Main
+{	
+	
+	public void cafeRubis()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new cafeRubis());
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.cafeRubis();			
+	}
+}
